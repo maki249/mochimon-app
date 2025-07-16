67cc2a68f05f65d2e7cee82299e71cc6057ca2a5
Tai <s2310137@wakayama-u.ac.jp>
2025-07-16 13:00:03 +0900

    削除の実装

--- Mochimon_mobile/EventEdit.js
+++ Mochimon_mobile/EventEdit.js
@@ -129,23 +129,24 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".cancel-button").addEventListener("click", () => {
         location.href = "Calendar.html";
     });
-    document.querySelector(".delete-button").addEventListener("click", async () => {
-    const user = auth.currentUser;
-    if (!user) return alert("再度ログインしてください");
-    if (!eventId) return alert("eventIdがありません");
+  
+  document.querySelector(".delete-button").addEventListener("click", async () => {
+  const user = auth.currentUser;
+  if (!user) return alert("再度ログインしてください");
+  if (!eventId) return alert("eventIdがありません");
 
-    const confirmed = confirm("この予定を本当に削除しますか？");
-    if (!confirmed) return;
+  const confirmed = confirm("この予定を本当に削除しますか？");
+  if (!confirmed) return;
 
-    try {
-        const ref = doc(db, "users", user.uid, "events", eventId);
-        await deleteDoc(ref);
-        alert("予定を削除しました");
-        location.href = "Calendar.html";  // 削除後に戻るページ
-    } catch (error) {
-        console.error("削除に失敗しました:", error);
-        alert("削除に失敗しました");
-    }
-    });
+  try {
+      const ref = doc(db, user.uid, eventId);
+      await deleteDoc(ref);
+      alert("予定を削除しました");
+      location.href = "Calendar.html";  // 削除後に戻るページ
+  } catch (error) {
+      console.error("削除に失敗しました:", error);
+      alert("削除に失敗しました");
+  }
+  });
 
 });

1c4ad7efbb1e3452149e3f9100d4cd37d6ccb909
Tai <s2310137@wakayama-u.ac.jp>
2025-07-30 13:57:54 +0900

    修正

--- Mochimon_mobile/UseTemplate.js
+++ Mochimon_mobile/UseTemplate.js
@@ -1,3 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getFirestore, addDoc, updateDoc, deleteDoc, collection, doc ,getDoc} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChPt5NDvgd4okxbUQalZtrS7w6Tm30fgg",
+    authDomain: "mochimon-base.firebaseapp.com",
+    projectId: "mochimon-base",
+    storageBucket: "mochimon-base.firebasestorage.app",
+    messagingSenderId: "5202457046",
+    appId: "1:5202457046:web:7233c6b556a7d260803477",
+    measurementId: "G-GPT541EW6S"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+let currentUser = null;
+const params = new URLSearchParams(window.location.search);
+const eventId = params.get("eventId");
+const date = params.get("date");
+
+// ユーザーの認証状態が変わるたびにcurrentUserにセット
+onAuthStateChanged(auth, async (user) => {
+    currentUser = user;
+    console.log("auth state changed:", user);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.small-card');
     const useButton = document.getElementById('use-button');
